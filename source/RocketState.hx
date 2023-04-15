f26936aaa470f27678c11d8e7e48059d9d778fcb
Kellie Behm <kb695@live.missouristate.edu>
2023-04-15 17:18:50 -0500

    Added typed text on computer

--- source/RocketState.hx
+++ source/RocketState.hx
@@ -1,9 +1,10 @@
 package;
 
-import flixel.FlxCamera;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.addons.text.FlxTypeText;
+import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.util.FlxColor;
 
@@ -11,6 +12,11 @@ class RocketState extends FlxState
 {
 	var quitbutton:FlxButton;
 	var computerscreen:FlxSprite;
+	var text1:FlxTypeText;
+	var text2:FlxTypeText;
+	var text3:FlxTypeText;
+	var text4:FlxTypeText;
+	var line:Int = 1;
 
 	override public function create()
 	{
@@ -21,20 +27,60 @@ class RocketState extends FlxState
 		computerscreen = new FlxSprite();
 		computerscreen.loadGraphic(AssetPaths.computer__png);
 
+		text1 = new FlxTypeText(75, 75, 500, "\nuser@8du7z35f> ", 20);
+		text1.prefix = "Press enter to proceed> ";
+		text1.delay = 0.1;
+
+		text2 = new FlxTypeText(75, 75, 500, "\n \nY/N? ", 20);
+		text2.delay = 0.1;
+
+		text3 = new FlxTypeText(75, 75, 500, "\n \n \nYou are not authorized to launch. ", 20);
+		text2.delay = 0.1;
+		text4 = new FlxTypeText(75, 75, 500, "\n \n \n \nPlease select launch coordinates", 20);
+		text2.delay = 0.1;
+
 		add(computerscreen);
 		add(quitbutton);
+		add(text1);
+		add(text2);
+		add(text3);
+		add(text4);
 		super.create();
 	}
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+		if (((FlxG.keys.pressed.ENTER) && (line == 1)))
+		{
+			text1.start();
+			++line;
+			trace(line);
+		}
+		if (((FlxG.keys.pressed.ENTER) && (line == 2)))
+		{
+			text2.start();
+			++line;
+			trace(line);
+		}
+		if (((FlxG.keys.pressed.Y) && (line == 3)))
+		{
+			text3.start();
+			++line;
+			trace(line);
+		}
+		if ((FlxG.keys.pressed.N && line == 3))
+		{
+			text4.start();
+			++line;
+			trace(line);
+		}
 	}
 
 	private function createQuitButton(labelText:String, onClick:Void->Void, posx:Int = 0, posy:Int = 0, color:FlxColor = FlxColor.BLACK):FlxButton
 	{
 		var button = new FlxButton(0, 0, labelText, onClick);
-		button.makeGraphic(20, 20, color);
+		button.makeGraphic(30, 30, color);
 		button.label.size = 10;
 		button.width = button.label.width;
 		button.height = button.label.height;
