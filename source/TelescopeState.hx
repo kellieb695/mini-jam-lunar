77b48466e0af7d5d9d49c9d69684a8b4a26427a0
Kellie Behm <kb695@live.missouristate.edu>
2023-04-16 13:51:28 -0500

    Added one planet and telescope shows coords

--- source/TelescopeState.hx
+++ source/TelescopeState.hx
@@ -2,12 +2,13 @@ package;
 
 import flixel.FlxCamera;
 import flixel.FlxG;
+import flixel.FlxObject;
 import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.FlxSubState;
+import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.util.FlxColor;
-import telescope.MoonHero;
 
 class TelescopeState extends FlxState
 {
@@ -16,6 +17,9 @@ class TelescopeState extends FlxState
 	var crosshair:FlxSprite;
 	var telecamera:FlxCamera;
 	var quitbutton:FlxButton;
+	var redPlanet:FlxSprite;
+	var crosshairPoint:FlxObject;
+	var coordText:FlxText;
 
 	override public function create()
 	{
@@ -27,6 +31,12 @@ class TelescopeState extends FlxState
 		crosshair = new FlxSprite();
 		crosshair.loadGraphic(AssetPaths.crosshair__png);
 		crosshair.scrollFactor.set(0);
+		redPlanet = new FlxSprite(700, 300);
+		redPlanet.loadGraphic(AssetPaths.redplanet__png);
+		crosshairPoint = new FlxObject(320, 240);
+		crosshairPoint.scrollFactor.set(0);
+		coordText = new FlxText(500, 240, 125, "X Coordinate: \nY Coordinate: ", 10);
+		coordText.scrollFactor.set(0);
 
 		quitbutton = createQuitButton("back", () ->
 		{
@@ -40,18 +50,33 @@ class TelescopeState extends FlxState
 		FlxG.cameras.reset(telecamera);
 
 		add(starrysky);
-		add(new MoonHero(150, 250));
-		add(new MoonHero(300, 150));
+		add(redPlanet);
 		add(starlayer);
 		add(crosshair);
 		add(quitbutton);
+		add(coordText);
 		super.create();
 	}
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+		scrollSky(elapsed);
+		showCoords();
+	}
 
+	private function createQuitButton(labelText:String, onClick:Void->Void, posx:Int = 0, posy:Int = 0, color:FlxColor = FlxColor.BLACK):FlxButton
+	{
+		var button = new FlxButton(posx, posy, labelText, onClick);
+		button.makeGraphic(40, 20, color);
+		button.label.size = 10;
+		button.scrollFactor.set(0);
+
+		return button;
+	}
+
+	private function scrollSky(elapsed:Float)
+	{
 		if (FlxG.mouse.getPosition().x > telecamera.scroll.x + 520)
 		{
 			telecamera.scroll.x += elapsed * 60;
@@ -73,13 +98,10 @@ class TelescopeState extends FlxState
 		}
 	}
 
-	private function createQuitButton(labelText:String, onClick:Void->Void, posx:Int = 0, posy:Int = 0, color:FlxColor = FlxColor.BLACK):FlxButton
+	private function showCoords()
 	{
-		var button = new FlxButton(posx, posy, labelText, onClick);
-		button.makeGraphic(40, 20, color);
-		button.label.size = 10;
-		button.scrollFactor.set(0);
-
-		return button;
+		var xcoord = crosshairPoint.getPosition().x + telecamera.scroll.x;
+		var ycoord = crosshairPoint.getPosition().y + telecamera.scroll.y;
+		coordText.text = "X Coordinate:" + xcoord + "\nY Coordinate:" + ycoord;
 	}
 }
